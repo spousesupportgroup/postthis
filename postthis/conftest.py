5d7d94dfcd22b3630cba7fb356ec61de24771012
Hendrik Baecker <andurin@process-zero.de>
2023-08-21 20:47:03 +0200

    After cookiecutter

---
+++ postthis/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from postthis.users.models import User
+from postthis.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
